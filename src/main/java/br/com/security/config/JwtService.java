a8e9cbd248e53e90832be564acff32c7c3c7cef1
natanaelsc <53065432+natanaelsc96@users.noreply.github.com>
2023-01-04 13:15:44 -0300

    fix: change method parseClaimsJwt to parseClaimsJws

--- src/main/java/br/com/security/config/JwtService.java
+++ src/main/java/br/com/security/config/JwtService.java
@@ -63,7 +63,7 @@ public class JwtService {
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
-                .parseClaimsJwt(token)
+                .parseClaimsJws(token)
                 .getBody();
     }
 
